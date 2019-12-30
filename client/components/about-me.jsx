715cfcb7bef8541471d8cd2ece96558ee6139bd4
kevinakahoshi <kevin.akahoshi@gmail.com>
2019-12-30 04:28:51 +0000

    Fixed random off characters

--- client/components/about-me.jsx
+++ client/components/about-me.jsx
@@ -59,11 +59,11 @@ function AboutMe() {
             <div className="position-relative heading-div">
               <h3 className="heading mb-4 spread">About Me</h3>
             </div>
-            <p>I’m Kevin Akahoshi, and I’m a developer that has a passion for technology and coding.</p>
-            <p>I’ve always had an interest in technology, and ever since I can remember I’ve been involved in computers. My first taste of programming came in the form of a few computer science courses at UC Riverside, which gave me some exposure to HTML and a little bit of C++. Through my professional experiences, I continued to find myself drawn to tech.</p>
-            <p>For the last four years, I’ve worked as an Account Manager in various organizations to help solve problems that our clients were having with technology. After working around many developers however, I decided to make the jump to fulfill my curiosity, and to pursue programming full time. I enrolled in an accelerated Full-Stack Web Development Program at LearningFuze.</p>
+            <p>I&#39;m Kevin Akahoshi, and I&#39;m a developer that has a passion for technology and coding.</p>
+            <p>I&#39;ve always had an interest in technology, and ever since I can remember I&#39;ve been involved in computers. My first taste of programming came in the form of a few computer science courses at UC Riverside, which gave me some exposure to HTML and a little bit of C++. Through my professional experiences, I continued to find myself drawn to tech.</p>
+            <p>For the last four years, I&#39;ve worked as an Account Manager in various organizations to help solve problems that our clients were having with technology. After working around many developers however, I decided to make the jump to fulfill my curiosity, and to pursue programming full time. I enrolled in an accelerated Full-Stack Web Development Program at LearningFuze.</p>
             <p>While enrolled in the program, I have learned languages like JavaScript (ES5 + ES6), React.js, jQuery, PHP, HTML, and CSS. I also became intimately familiar with tools like Git/Github, Postman, npm, and Webpack. Lastly, I participated in team-based hackathons, as well as solo projects, to build full-stack web applications. Experiencing all of this has solidified that I have made the right decision to pursue web development, and has helped feed my curiosity.</p>
-            <p>Because I have years of experience working with clients, and seeing things from their point of view, I bring a unique perspective to the conversation. I’m the Account Manager gone rogue.</p>
+            <p>Because I have years of experience working with clients, and seeing things from their point of view, I bring a unique perspective to the conversation. I&#39;m the Account Manager gone rogue.</p>
           </Col>
         </Row>
       </Container>
